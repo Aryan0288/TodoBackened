30584cbe86bf3d5cc584952b091d632629eb6b34
Aryan <102355469+Aryan0288@users.noreply.github.com>
2024-04-13 17:39:09 +0000

    dotenv

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express=require("express");
 const app = express();
-const port = 4000;
 const cors=require("cors");
+const dotenv=require('dotenv');
+dotenv.config();
+const port = 4000 || process.env.PORT
+
 app.use(express.json());
 
 app.use(
